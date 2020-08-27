5cfcaabd9c411a58061bb0b103ebf159211cfbd4
Johan Arias <johan.arias@mail.escuelaing.edu.co>
2020-08-26 23:59:29 -0500

    Adding profile user view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Main from './component/Main/Main';
 import SignUp from './component/signup/SignUp';
 import { SignIn } from './component/signin/SignIn';
 import UserProfile from './component/Profile/UserProfile'
@@ -10,9 +9,6 @@ function App() {
   return (
    <BrowserRouter>
     <Switch>
-
-        <Route path="/" 
-            render={()=><Main/>} exact/>
 
         <Route path="/signup" 
             render={()=><SignUp/>} exact/>
---
+++ src/component/Profile/Mytext.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField';
+import { Button } from '@material-ui/core';
+
+export class Mytext extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {isEditable : false,person:""};
+        this.state.person = {username:"johan",email:"johan@mail.com",creditsCard:"1233-3231-23313",cellphone:"2323232",address:"Kr 9 .."};   
+        this.handleChangeField = this.handleChangeField.bind(this);
+        this.handleChangeEditable = this.handleChangeEditable.bind(this);
+    }
+
+    handleChangeField(e) {
+        this.state.person[this.props.field] = e.target.value;
+        this.setState(this.state)
+    }
+
+    handleChangeEditable() {
+        this.state.isEditable = !this.state.isEditable;
+        this.setState(this.state)
+    }
+
+
+    render(){
+        return (
+        <div>
+            <div style={{display: "flex", height:'55px', flexDirection : "raw",borderStyle:'outset'}}>
+                <div style={{width:'550px',display:'flex',alignItems:'center',justifyContent:'center'}}>
+
+                <div style={{width:'40%',height:'60%', alignItems:'left',background:'#1976d2',color:'white',textAlign:'center'}}>
+                    <label >
+                        {this.props.title}
+                    </label>
+                </div>
+                <div style={{width:'50%',paddingLeft: '50px'}}>
+                { this.state.isEditable ? 
+                <TextField 
+                    value = {this.state.person[this.props.field]}
+                    type="text"
+                    onChange={(e) => this.handleChangeField(e)}
+                >
+                </TextField>: 
+                <label>
+                    {this.state.person[this.props.field]}
+                </label>
+                }
+                </div>
+                </div>
+                <div>
+                    <Button 
+                        variant="contained" 
+                        color="primary"
+                        onClick={()=> {this.handleChangeEditable()}}>
+                        {this.state.isEditable ? "Save" : "Edit"}
+                    </Button>
+                </div>
+
+            </div>
+        </div>
+        );
+
+    }
+
+
+
+}

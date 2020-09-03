44c37df56ddc5737cf5a152ae2ee717df52d6f22
Andres Marcelo <felipemarcelo156@gmail.com>
2020-09-03 03:56:13 -0500

    Mejora al catáogo
    Se refactorizo la vista del catalogo para que recibiera los parametros desde un arrelo y ademas cargara cada componente del drawer  por categorías y cuando se hace click se dirige exactamente a  ese componente.

---
+++ src/component/catalog/Category.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Card } from "@material-ui/core";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import { ListProduct } from "./ListProduct";
+
+export class Category extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <a name={this.props.name} id={this.props.name}>
+        <Grid container spacing={1}>
+          <Typography variant="h6" noWrap>
+            {this.props.name}
+          </Typography>
+
+          <Grid
+            container
+            spacing={1}
+            direction="row"
+            justify="center"
+            alignItems="baseline"
+          >
+            {this.props.products}
+          </Grid>
+        </Grid>
+      </a>
+    );
+  }
+}

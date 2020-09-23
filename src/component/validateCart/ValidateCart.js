a9ffc963913ccf0730083997667a1e278c35ba54
Johann Paez <najoh2907@hotmail.com>
2020-09-23 17:52:40 -0500

    Validate Cart

--- src/component/validateCart/ValidateCart.js
+++ src/component/validateCart/ValidateCart.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppBar from "../appbar/AppBar";
 import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
@@ -12,7 +11,21 @@ import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import FavoriteIcon from '@material-ui/icons/Favorite';
+import arroz from './static/images/arroz.jpg';
+import chocolate from './static/images/chocolate.jpg';
+import huevos from './static/images/huevos.jpg';
+import lentejas from './static/images/lentejas.png';
+import limon from './static/images/limon.jpg';
 import paella from './static/images/paella.jpg';
+import zapatos from './static/images/zapatosHombre.jpg';
+import cafe from './static/images/cafe.jpg';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import CloseIcon from '@material-ui/icons/Close';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const useStylesAlt = makeStyles((theme) => ({
     root: {
@@ -35,38 +48,76 @@ const useStyles = makeStyles((theme) => ({
   }  
 }));
 
+function AlertDialog(props) {
+  
 
+  return (
+    <div>      
+      <Dialog
+        open={props.open}
+        onClose={props.handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"¿Eliminar producto?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            ¿Está seguro que desea eliminar el producto? 
+            Este cambio no podrá ser anulado.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => props.handleClose()} color="primary">
+            Cancelar
+          </Button>
+          <Button onClick={() => props.handleClose(), () => props.deleteProduct(props.id)} color="secondary" autoFocus>
+            Eliminar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
 
-function NestedGrid() {
+function NestedGrid(props) {
     const classes2 = useStylesAlt();
 
-    function RecipeReviewCard() {
+    function RecipeReviewCard(props) {
       const classes = useStyles();
-      
-    
+      const [open, setOpen] = React.useState(false);
+      const handleClickOpen = () => {
+        setOpen(true);
+      };
+
+      const handleClose = () => {
+        setOpen(false);
+      };
       return (
-        <Grid item xs={3}>
+        
+        <Grid item xs={2}>
         <Card className={classes.root}>
-          <CardHeader                
-            title="Shrimp and Chorizo Paella"
-            subheader="September 14, 2016"
+          <CardHeader    
+            action={
+              <IconButton aria-label="settings">
+                <CloseIcon onClick = {handleClickOpen}/>
+              </IconButton>
+                
+            }            
+            title= {props.name}
+            subheader= {"$ " + props.price}
           />
+          <AlertDialog open ={open} handleClickOpen = {handleClickOpen} handleClose = {handleClose}
+            deleteProduct = {props.deleteProduct} id = {props.id}/>
           <CardMedia
             className={classes.media}
-            image={paella}
+            image={props.ruta}
             title="Paella dish"
           />
           <CardContent>
             <Typography variant="body2" color="textSecondary" component="p">
-              This impressive paella is a perfect party dish and a fun meal to cook together with your
-              guests. Add 1 cup of frozen peas along with the mussels, if you like.
+              {props.description}
             </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="add to favorites">
-              <FavoriteIcon />
-            </IconButton>        
-          </CardActions>      
+          </CardContent>               
         </Card>
         </Grid>
       );
@@ -75,12 +126,18 @@ function NestedGrid() {
   
     return (
       <div className={classes2.root}>
-        <Grid container spacing={1}>
+        <Grid container spacing={0}>
           <Grid container item xs={12} spacing={3}>
-            <RecipeReviewCard/>
-          </Grid>
-          <Grid container item xs={12} spacing={3}>
-            <RecipeReviewCard/>
+            {props.items.map((item, index) =>               
+              <RecipeReviewCard 
+                    id = {item.id}
+                    key = {item.name + "_" + index} 
+                    name = {item.name} 
+                    price = {item.price} 
+                    description = {item.description} 
+                    ruta = {item.ruta}
+                    deleteProduct = {props.deleteProduct}/>
+            )}
           </Grid>
         </Grid>
       </div>
@@ -91,16 +148,25 @@ export default class ValidateCart extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {productsCart: [{name:"Limón",price:"2.000",description:"_"},
-                                    {name:"Pasta",price:"2.000",description:"_"},
-                                    {name:"Arroz",price:"2.000",description:"_"},
-                                    {name:"Salchicha",price:"2.000",description:"_"},
-                                    {name:"Platano",price:"2.000",description:"_"},
-                                    {name:"Papa",price:"2.000",description:"_"},
-                                    {name:"Huevos",price:"2.000",description:"_"},
-                                    {name:"Chicharron",price:"2.000",description:"_"},
-                                    {name:"Cafe",price:"2.000",description:"_"},
-                                    {name:"Lentejas",price:"2.000",description:"_"}]};        
+        this.state = {productsCart: [{id: 1, name:"Limón",price:"1.000",description:"Cuatro limones.", ruta: limon},
+                                    {id: 2, name:"Arroz",price:"10.800",description:"Cinco kilos de arroz ROA.", ruta: arroz},
+                                    {id: 3, name:"Huevos",price:"2.000",description:"Una docena de huevos.", ruta: huevos},
+                                    {id: 4, name:"Chocolate",price:"4.000",description:"Barra de chocolate Hershey's", ruta: chocolate},
+                                    {id: 5, name:"Cafe",price:"4.300",description:"Cinco libras de café sello rojo.", ruta: cafe},
+                                    {id: 6, name:"Zapatos",price:"70.000",description:"Zapatos formales para hombre.", ruta: zapatos},
+                                    {id: 7, name:"Paella",price:"14.000",description:"Paella es tamaño grande.", ruta: paella},
+                                    {id: 8, name:"Lentejas",price:"3.000",description:"Una libra de lentejas. ", ruta: lentejas}]};        
+
+        this.deleteProduct = this.deleteProduct.bind(this);                                    
+    }
+
+    deleteProduct(id) {
+      var newList = []
+      for (var i = 0; i < this.state.productsCart.length; i++) {
+        var item = this.state.productsCart[i];
+        if (item.id !== id) newList.push(item);
+      }
+      this.setState({productsCart: newList})
     }
 
     render() {    
@@ -109,16 +175,19 @@ export default class ValidateCart extends React.Component {
             <div>
                 <AppBar/>                
                 <div style = {{height: '80px', width: '100%'}}></div>
-                <div>
+                <div style = {{textAlign: 'center'}}>
                     <header>
                         <h1> Validar Productos </h1>
                     </header>
                 </div>
-                <NestedGrid></NestedGrid>
-                <br></br><br></br><br></br>
-                <Button variant="contained" color="primary" style = {{height: '40px', borderRadius: '4px', position: 'inline-block'}}>
-                    Validar Carrito
-                </Button>             
+                <div style = {{paddingLeft: '50px', paddingRight: '50px'}}>
+                  <NestedGrid items = {this.state.productsCart} deleteProduct = {this.deleteProduct}></NestedGrid>
+                
+                  <br></br><br></br><br></br>
+                  <Button variant="contained" color="primary" style = {{height: '40px', borderRadius: '4px', position: 'inline-block'}}>
+                      Validar Carrito
+                  </Button>             
+                </div>
             </div>
         );
     }

aea678a9edb5285c9933970c98a8a2f7f2058ecf
Andres Marcelo <felipemarcelo156@gmail.com>
2020-10-22 19:10:05 -0500

    Adding connection with backend

--- src/component/Order/UserOrders.js
+++ src/component/Order/UserOrders.js
@@ -3,21 +3,13 @@ import { DataGrid } from '@material-ui/data-grid';
 import AppBar from "../appbar/AppBar";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Divider from "@material-ui/core/Divider";
-import Drawer from "@material-ui/core/Drawer";
-import Hidden from "@material-ui/core/Hidden";
-import IconButton from "@material-ui/core/IconButton";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
-import MailIcon from "@material-ui/icons/Mail";
-import MenuIcon from "@material-ui/icons/Menu";
-import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { Link, animateScroll as scroll } from "react-scroll";
 import LocalOfferIcon from "@material-ui/icons/LocalOffer";
-import { Todo } from "../todo/Todo";
 import { withStyles } from "@material-ui/core/styles";
 
 import { getUrl } from "../../vars";
@@ -42,21 +34,6 @@ const rows = [
   { id: 8, tienda: 'Snow', valortotal: 4522,fecha:"2020-01-01",estado:"completado"  },
   { id: 9, tienda: 'Snow', valortotal: 4522,fecha:"2020-01-01",estado:"completado"  },
 ];
-
-
-
-export class DataTable extends React.Component {
-    render(){
-        return (
-            <div style={{ height: 400, width: '100%'}}>
-              <DataGrid rows={rows} columns={columns} pageSize={5} />
-              
-            </div>
-          );
-    }
-  
-}
-
 
 const drawerWidth = 240;
 
@@ -96,20 +73,58 @@ const useStyles = (theme) => ({
 export class UserOrders extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { mobileOpen: false, productsCart: [],products:[] };
+    this.state = { mobileOpen: false, redirect:false };
     console.log(props == undefined);
+  }
+
+  componentDidMount(){
+      this.getMyOrders().then(() => this.setState({redirect:true}));
+    
+  }
+
+    parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
+
+  getMyOrders(){
+    let url = getUrl();
+    let token = localStorage.getItem("token");
+    const headers = {
+      Authorization:
+        token,
+    };
+    let user = this.parseJwt(token);
+    
+    let a = new Promise((resolve,reject) => {
+        fetch(url + "/orders/user/"+user.sub, {
+            headers: headers,
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data);
+              rows = {id:data.id,tienda:data.shop,valortotal:data.total,fecha:data.date}
+              resolve();
+            });
+    });
+    return a;
+
   }
 
   render() {
    const testList = this.state.products;
     const { window } = this.props;
     const { classes } = this.props;
-    console.log("clases::........");
     console.log(this.classes);
     const handleDrawerToggle = () => {
       this.setState({ mobileOpen: !this.state.mobileOpen });
     };
-    document.title = "Ieti deep | " + this.props.store;
+    document.title = "Ieti deep | Mis productos";
 
     const drawer = (
       <div>
@@ -147,34 +162,37 @@ export class UserOrders extends React.Component {
 
     const container =
       window !== undefined ? () => window().document.body : undefined;
-
-    return (
-      <div className={classes.root}>
-        <AppBar
-          sumAmount={this.sumAmount}
-          removeProduct={this.removeProduct}
-          productsCart={this.state.productsCart}
-          removeAllProductsCart={this.removeAllProductsCart}
-        />
-
-        <div style={{ height: "7  0px" }}></div>
-        <CssBaseline />
-        
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <center>
-            <Typography gutterBottom variant="h3" component="h2">
-              {"Mis Ordenes"}
-            </Typography>
-          </center>
-          <div style={{ height: 400, width: '80%',margin: "0 auto"}}>
-              <DataGrid rows={rows} columns={columns} pageSize={5} />
+      
+    if (this.state.redirect){
+        return (
+            <div className={classes.root}>
+              <AppBar
+                sumAmount={this.sumAmount}
+                removeProduct={this.removeProduct}
+                productsCart={this.state.productsCart}
+                removeAllProductsCart={this.removeAllProductsCart}
+              />
+      
+              <div style={{ height: "7  0px" }}></div>
+              <CssBaseline />
               
+              <main className={classes.content}>
+                <div className={classes.toolbar} />
+                <center>
+                  <Typography gutterBottom variant="h3" component="h2">
+                    {"Mis Ordenes"}
+                  </Typography>
+                </center>
+                <div style={{ height: 400, width: '80%',margin: "0 auto"}}>
+                    <DataGrid rows={rows} columns={columns} pageSize={5} />
+                    
+                  </div>
+              </main>
             </div>
-          {/* Aqui hay que poner la lista de product con los porps que son las categorias */}
-        </main>
-      </div>
-    );
+          );
+    }
+    else{return(<h1>Cargando Data</h1>)}
+    
   }
 
   
